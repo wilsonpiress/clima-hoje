55c0d56d7f8bd8695c8f0c3e61313853ea61b92a
wilsonpiress <wilsonpires.js@gmail.com>
2022-11-10 15:31:32 -0300

    Função que traz os dados da api para aplicação

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,8 +14,18 @@ const umidityElement = document.querySelector("#umidity span");
 const windElement = document.querySelector("#wind span");
 
 // Funções
+const getWeatherData = async (city) => {
+
+    const apiWeatherURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}&lang=pt_br`;
+
+    const res = await fetch(apiWeatherURL);
+    const data = await res.json();
+
+    console.log(data);
+};
+
 const showWeatherData = (city) => {
-    console.log(city);
+    getWeatherData(city);
 };
 
 // Eventos
