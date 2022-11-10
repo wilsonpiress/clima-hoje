85d73c5d8d0e9545c72d9d29da2e68c05371d70d
wilsonpiress <wilsonpires.js@gmail.com>
2022-11-10 15:03:22 -0300

    Selecionei elementos para preenchimento dos dados

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,17 @@
 // Variáveis e seleção de elementos
+
 const apiCountryURL = "https://countryflagsapi.com/png/";
 
 const cityInput = document.querySelector("#city-input");
 const searchBtn = document.querySelector("#search");
+
+const cityElement = document.querySelector("#city");
+const tempElement = document.querySelector("#temperature span");
+const descElement = document.querySelector("#description");
+const weatherIconElement = document.querySelector("#weather-icon");
+const countryElement = document.querySelector("#country");
+const umidityElement = document.querySelector("#umidity span");
+const windElement = document.querySelector("#wind span");
 
 // Funções
 const showWeatherData = (city) => {
